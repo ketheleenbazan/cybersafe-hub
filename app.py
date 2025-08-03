43cc36cc577dac6007f6b5cd75c3ace868dadaf1
ketheleenbazan <ketheleenbazan@gmail.com>
2025-08-03 15:35:37 +0100

    troubleshooting the use Heroku PORT environment variable

--- app.py
+++ app.py
@@ -293,4 +293,5 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
